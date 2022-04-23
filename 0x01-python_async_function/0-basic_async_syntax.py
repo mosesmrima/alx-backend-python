c76df8a45f8fa2ab7fa4c645c11b5ea387de5ebc
mosesmrima <mrimamss@gmail.com>
2022-04-23 22:26:39 +0300

    wait

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" function that waits for a random number of seconds """
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    asynchronous coroutine that
+    waits for a random delay between 0 and max_delay
+    :param max_delay: maximum number of seconds to wait
+    return: number of seconds waited
+    """
+    t = random.uniform(0, max_delay)
+    await asyncio.sleep(t)
+    return t
